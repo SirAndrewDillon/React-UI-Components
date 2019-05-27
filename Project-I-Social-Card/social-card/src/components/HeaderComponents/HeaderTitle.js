04e572e2c4c26ef2d9aec1a00d9e354228d33906
Andrew Dillon <majik895@gmail.com>
2019-05-27 17:39:04 -0400

    HeaderTitle.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeadedrTitle = () => {
+    return (
+        <div class = "headerTitle">
+            <h4>Lambda School</h4>
+            <span>@lambdaSchool</span>
+            <span>28 May</span>
+        </div>
+    );
+}
